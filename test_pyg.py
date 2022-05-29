de8358d5bac3c4ae330a228829aa9bf4986d7940
Ira Rickman <irarickman@gmail.com>
2022-05-29 16:48:44 -0700

    test

--- test_pyg.py
+++ test_pyg.py
@@ -4,7 +4,7 @@ from google.oauth2 import service_account
 
 
 cred_file=os.environ.get("GOOGLE_GHA_CREDS_PATH")
-
+SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
 credentials = service_account.Credentials.from_service_account_file(
         cred_file, scopes=SCOPES)
 gc = pygsheets.authorize(custom_credentials =credentials) 
